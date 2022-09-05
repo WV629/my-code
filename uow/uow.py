f38f3ae289cdfca9523ff7f83ed0d444b2254cbf
Mr_WV <76678353+2762882807@users.noreply.github.com>
2022-09-05 15:24:15 +0800

    Update uow.py

--- uow/uow.py
+++ uow/uow.py
@@ -13,7 +13,11 @@ def parse(li,faculy,degrees,url):
     list1['course_name'] = li.xpath(".//a/text()")[0]
     list1['href'] = li.xpath(".//a/@href")[0]
     # print(list1['href'])
-    html = etree.HTML(requests.get(list1['href']).content.decode())
+    res = requests.get(list1['href']).content.decode()
+    # print(res)
+    if 'Course not found' in res:
+        return
+    html = etree.HTML(res)
     list1['desc'] = ''.join(
         [i.strip() for i in html.xpath("//h2[text()='Course summary']/following-sibling::div//text()")])
     list1['study area'] = ''.join(html.xpath("//p[text()='Study area']/following-sibling::p/text()")).strip()
@@ -25,12 +29,24 @@ def parse(li,faculy,degrees,url):
         html.xpath("//*[text()='Career opportunities']/following-sibling::ul/li/text()")).strip()
     print(list1)
     print("*" * 50)
-    trs = html.xpath("//h4[text()='FEES']/following-sibling::div//tbody/tr")
-    for tr in trs:
-        list1['international_location'] = tr.xpath("./td[1]/p/text()")[0]
-        list1['international_mode'] = tr.xpath("./td[2]/p/text()")[0]
-        list1['international_session_fee'] = tr.xpath("./td[3]/p/text()")[0]
-        list1['international_full'] = tr.xpath("./td[4]/p/text()")[0]
+    trs = html.xpath("//h4[@id='intl-fees']/following-sibling::div//table/tr")
+    print(len(trs))
+    if len(trs) != 0 :
+        for tr in trs:
+            list1['international_location'] = tr.xpath("./td[1]/p/text()")[0]
+            list1['international_mode'] = tr.xpath("./td[2]/p/text()")[0]
+            list1['international_session_fee'] = tr.xpath("./td[3]/p/text()")[0]
+            list1['international_full'] = tr.xpath("./td[4]/p/text()")[0]
+    list1['international_csp'] = ''.join(html.xpath("//h4[@id='intl-fees']/following-sibling::div/div/p//text()"))
+
+    trs = html.xpath("//h4[@id='dom-fees']/following-sibling::div//table/tr")
+    print(len(trs))
+    if len(trs) != 0:
+        for tr in trs:
+            list1['domestic_location'] = tr.xpath("./td[1]/p/text()")[0]
+            list1['domestic_mode'] = tr.xpath("./td[2]/p/text()")[0]
+            list1['domestic_session_fee'] = tr.xpath("./td[3]/p/text()")[0]
+            list1['domestic_full'] = tr.xpath("./td[4]/p/text()")[0]
     list1['domestic_csp'] = ''.join(html.xpath("//h4[@id='dom-fees']/following-sibling::div/p//text()"))
     datas.append(list1)
 
@@ -55,4 +71,3 @@ if __name__ == '__main__':
         [t.join() for t in th]
     df = pd.DataFrame(datas)
     df.to_excel("uow.xlsx",index=False)
-
